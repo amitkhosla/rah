9cdf5dbb40cb9a59160fa96963a32f80867a92d0
Amit Khosla <amitkhosla.jobs@gmail.com>
2026-02-28 20:44:16 +0530

    Revert cache hash to xxh3 implementation

--- internal/cache/hash.go
+++ internal/cache/hash.go
@@ -2,26 +2,18 @@ package cache
 
 import (
 	"encoding/binary"
-	"hash/fnv"
+
+	"github.com/zeebo/xxh3"
 )
 
 func Hash128(tenantID uint16, key []byte) [16]byte {
-	// Seed with tenant id and derive two independent 64-bit digests.
-	seed := []byte{byte(tenantID), byte(tenantID >> 8)}
 
-	h1 := fnv.New64a()
-	h1.Write(seed)
-	h1.Write(key)
-	lo := h1.Sum64()
-
-	h2 := fnv.New64()
-	h2.Write(seed)
-	h2.Write(key)
-	hi := h2.Sum64()
+	// Seed the hash with the tenantID to provide isolation
+	sum := xxh3.Hash128Seed(key, uint64(tenantID))
 
 	var out [16]byte
-	binary.LittleEndian.PutUint64(out[0:8], lo)
-	binary.LittleEndian.PutUint64(out[8:16], hi)
+	binary.LittleEndian.PutUint64(out[0:8], sum.Lo)
+	binary.LittleEndian.PutUint64(out[8:16], sum.Hi)
 
 	return out
 }
